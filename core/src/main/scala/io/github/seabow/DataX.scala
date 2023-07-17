83ef734419de0c1990adbc81011f6322c2ec36ed
arlixu <289964653@qq.com>
2023-07-17 10:35:11 +0800

    0.2.6 snapshot

--- core/src/main/scala/io/github/seabow/DataX.scala
+++ core/src/main/scala/io/github/seabow/DataX.scala
@@ -14,6 +14,7 @@ object DataX extends Logging {
       .set("spark.port.maxRetries", "30")
       .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .set("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2")
+      .set("spark.hadoop.mapreduce.fileoutputcommitter.cleanup.skipped", "true")
       .set("spark.sql.broadcastTimeout", "3000")
     //获取参数
     val params = ParameterUtils.getParameters(args)
