0035d8a958c13f68755699181a77f4a4bb3d625f
arlixu <289964653@qq.com>
2024-03-07 14:30:32 +0800

    bugfix and add log

--- common/src/main/scala/io/github/seabow/datax/common/IcebergUtils.scala
+++ common/src/main/scala/io/github/seabow/datax/common/IcebergUtils.scala
@@ -5,6 +5,7 @@ import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.iceberg.CatalogUtil
 import org.apache.iceberg.catalog.TableIdentifier
 import org.apache.iceberg.spark.actions._
+import org.apache.spark.internal.Logging
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types.{DataType, StructType}
 
@@ -15,29 +16,33 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext, Future}
 
-object IcebergUtils{
+object IcebergUtils extends Logging{
   def getPartitionSpecAndLocation(table:String):(String,String)={
     val spark=SparkSession.getActiveSession.get
     val describeInfo=spark.sql(s"describe EXTENDED $table").collect()
     val location=describeInfo.filter(_.getAs[String]("col_name").equals("Location")).head.getAs[String]("data_type")
     val partitionSpecDDL=describeInfo.filter(_.getAs[String]("col_name").equals("_partition")).head.getAs[String]("data_type")
     val partitionSpec=DataType.fromDDL(partitionSpecDDL).asInstanceOf[StructType].fieldNames.mkString(",")
+    log.warn(s"partitionSpec : [$partitionSpec] , Location : $location")
     (partitionSpec,location)
   }
-   def expireSnapshotsWithSpark(tableName:String): Unit ={
+   def expireSnapshotsWithSpark(tableName:String,catalogType:String): Unit ={
      val tableNamePart=tableName.split("\\.")
      val catalogName=tableNamePart.head
      val namespace=tableNamePart(1)
      val shortTableName=tableNamePart.last
-   val catalog=CatalogUtil.buildIcebergCatalog(catalogName,Map("type"->"hive").asJava,SparkSession.getActiveSession.get.sparkContext.hadoopConfiguration)
+     val sparkContext=SparkSession.getActiveSession.get.sparkContext
+   val catalog=CatalogUtil.buildIcebergCatalog(catalogName,Map("type"->catalogType).asJava,sparkContext.hadoopConfiguration)
      catalog.initialize(catalogName,Map("type"->"hive").asJava)
     val table= catalog.loadTable(TableIdentifier.of(namespace, shortTableName))
     val action=SparkActions.get.expireSnapshots(table).expireOlderThan(System.currentTimeMillis()
     ).retainLast(1)
      //expire metadata
-     val filesToClear=action.expireFiles()
+     var filesToClear=action.expireFiles().repartition(30).cache()
+     var clearFileCnt=filesToClear.count()
      // clear file using spark
-     filesToClear.repartition(30).foreachPartition{
+     sparkContext.setJobDescription(s"expire snapshots: deleting $clearFileCnt files.")
+     filesToClear.foreachPartition{
        partition: Iterator[FileInfo]=>
          val clearTasks: ListBuffer[Future[Any]] = ListBuffer.empty
          val executor = Executors.newFixedThreadPool(20) // 这里设置了最大线程数为20
@@ -54,5 +59,6 @@ object IcebergUtils{
          Await.result(Future.sequence(clearTasks),Duration.Inf)
          println("Partition Done")
      }
+     filesToClear.unpersist(true)
    }
 }
--- core/src/main/scala/io/github/seabow/datax/core/pipeline/processor/IcebergLifecycleProcessor.scala
+++ core/src/main/scala/io/github/seabow/datax/core/pipeline/processor/IcebergLifecycleProcessor.scala
@@ -6,38 +6,71 @@ import io.github.seabow.datax.core.pipeline.Processor
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.DataFrame
 
+import java.util.concurrent.Executors
+import java.util.concurrent.atomic.AtomicInteger
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.concurrent.duration._
-object IcebergLifecycleProcessorConfig {
 
+object IcebergLifecycleProcessorConfig {
+  val concurrent_clear_tables = "concurrent_clear_tables"
+  val catalog_name = "catalog_name"
+  val catalog_type = "catalog_type"
 }
 
 class IcebergLifecycleProcessor extends Processor with Logging {
 
-  override def process(dfList: ListBuffer[DataFrame]): DataFrame = {
-    // 顺序清理
-    val inputDF=dfList.head
-    inputDF.collect().foreach{
-      row=>
-        //步骤一:执行delete操作
-        val tableName=row.getAs[String]("table_name")
-        val retain_days= row.getAs[Int]("retain_days")
-        val (partitionSpec,location)=IcebergUtils.getPartitionSpecAndLocation(tableName)
-        val needToClearDir=HdfsUtils.listDirs(location).filter(
-          System.currentTimeMillis()-_.getModificationTime>retain_days.day.toMillis).map(_.getPath.toString)
-        val inPartitionValues= needToClearDir.map(_.split("=").last).map("'"+_+"'").mkString(",")
-        val topPartitionCol=partitionSpec.split(",").head
-        val clearSql=s"delete from $tableName where $topPartitionCol in ($inPartitionValues)"
-        spark.sql(clearSql)
-        //步骤二.执行expireSnapshots操作
-          IcebergUtils.expireSnapshotsWithSpark(tableName)
-        //步骤三.删除目录。
-        //TODO 这里是否要加一个分布式删除？
-        needToClearDir.foreach{
-          dir=>
+  def clearTable(tableName: String, retainDays: Int, catalogType: String): Unit = {
+    val (partitionSpec, location) = IcebergUtils.getPartitionSpecAndLocation(tableName)
+    val needToClearDir = HdfsUtils.listDirs(location+"/data").filter(
+      System.currentTimeMillis() - _.getModificationTime > retainDays.day.toMillis).map(_.getPath.toString)
+    log.warn(s"$tableName need to clear ${needToClearDir.size} directories.")
+    if (needToClearDir.nonEmpty) {
+      val inPartitionValues = needToClearDir.map(_.split("=").last).map("'" + _ + "'").mkString(",")
+      val topPartitionCol = partitionSpec.split(",").head
+      val clearSql = s"delete from $tableName where $topPartitionCol in ($inPartitionValues)"
+      spark.sparkContext.setJobDescription(clearSql)
+      spark.sql(clearSql)
+      //步骤二.执行expireSnapshots操作
+      spark.sparkContext.setJobDescription(s"expireSnapshotsWithSpark : tableName $tableName, catalogType $catalogType")
+      IcebergUtils.expireSnapshotsWithSpark(tableName, catalogType)
+      //步骤三.删除目录。
+      spark.sparkContext.setJobDescription(s"clear ${needToClearDir.size} dirs:${needToClearDir.mkString(",")}")
+      spark.sparkContext.parallelize(needToClearDir).foreach {
+        dir =>
           HdfsUtils.delete(dir)
-        }
+      }
     }
+  }
+
+  override def process(dfList: ListBuffer[DataFrame]): DataFrame = {
+    val catalog_name = config.getString(IcebergLifecycleProcessorConfig.catalog_name, "iceberg")
+    val catalog_type = config.getString(IcebergLifecycleProcessorConfig.catalog_type, "hive")
+    val concurrent_clear_tables = config.getInt(IcebergLifecycleProcessorConfig.concurrent_clear_tables, 5)
+    val inputDF = dfList.head
+    val executor = Executors.newFixedThreadPool(concurrent_clear_tables) // 这里设置了最大线程数为20
+    val ec = ExecutionContext.fromExecutorService(executor)
+    val clearTasks = ListBuffer.empty[Future[Any]]
+    val tablesClearInfos = inputDF.collect()
+    val totalSize = tablesClearInfos.size
+    val currentClearIndex = new AtomicInteger(1)
+    tablesClearInfos.foreach {
+      row =>
+        //步骤一:执行delete操作
+        var tableName = row.getAs[String]("table_name")
+        if (tableName.split("\\.").size == 2) {
+          tableName = catalog_name + "." + tableName
+        }
+        val retain_days = row.getAs[Int]("retain_days")
+        val clearTask = Future {
+          val jobGroup=s"$tableName (${currentClearIndex.getAndIncrement()}/$totalSize)"
+          spark.sparkContext.setJobGroup(jobGroup, s"start clear $jobGroup")
+          clearTable(tableName, retain_days, catalog_type)
+        }(ec)
+        clearTasks.append(clearTask)
+    }
+    Await.result(Future.sequence(clearTasks), Duration.Inf)
     spark.emptyDataFrame
   }
 
